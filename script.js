73ea57bb661a9812a6312cf4787a50d90cac722f
Akash Madavi <akashmadavi2001@gmail.com>
2025-04-20 07:54:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let card = document.querySelector('.container');
+
+card.addEventListener('mousemove', (e) => {
+    let rect = card.getBoundingClientRect();
+    let x = e.clientX - rect.left - rect.width / 2;
+    let y = e.clientY - rect.top - rect.height / 2;
+
+    let rotateX = (y / rect.width) * -30;
+    let rotateY = (x / rect.height) * 30;
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+    let bgX = (x / rect.width) * 50 + 50;
+    card.style.backgroundPositionX = `${bgX}%`;
+});
+
+card.addEventListener('mouseleave', () => {
+    card.style.transform = `perspective(10000px) rotateX(0deg) rotateY(0deg)`;
+    card.style.backgroundPositionX = ``;
+});
